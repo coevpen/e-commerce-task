3721c3ebdfa0e100df9cb23bdb059b284359b790
coevpen <koinuevins@gmail.com>
2022-02-04 22:47:51 -0600

    all routes created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,14 +50,54 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(categoryInfo => res.json(categoryInfo))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryInfo => {
+    if(!categoryInfo){
+      res.status(404).json({ message: 'No category found. '});
+      return;
+    }
+    res.json(categoryInfo);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryInfo => {
+    if(!categoryInfo){
+      res.status(404).json({ message: 'No category found. '});
+      return;
+    }
+    res.json(categoryInfo);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
